7be5c8fd70e8ab7fbe9d4e7a0b02f0bad2b49eaa
Inna Romankova <romankova03071978@gmail.com>
2021-11-03 22:36:05 +0300

    Java file EmployeesInsertNames for inserting values into table Employees in PostgreSQl are added

---
+++ SQL/InsertValuesIntoTableEmployees/src/main/java/postgreSQL/EmployeesInsertNames.java
@@ -0,0 +1,61 @@
+package postgreSQL;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class EmployeesInsertNames {
+    public static void main(String[] args) {
+        //Загружаем драйвер
+        try {
+            Class.forName ("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace ();
+        }
+        //Получаем файл со свойствами
+        File fileDB = new File ("src/main/resources/db.properties");
+        //Создаем объект Properties и загружаем в него данные из файла
+        Properties propertiesDB = new Properties ();
+        try {
+            propertiesDB.load (new FileReader (fileDB));
+        } catch (IOException e) {
+            e.printStackTrace ();
+        }
+        try {
+            //Устанавливаем соедиенение
+            Connection conn = DriverManager.getConnection (propertiesDB.getProperty ("URL"),
+                    propertiesDB.getProperty ("USER"), propertiesDB.getProperty ("PWD"));
+
+            //Создаем объект запроса
+            Statement stmt = conn.createStatement ();
+
+
+            //Получаем файл со свойствами
+            File file = new File ("src/main/resources/employees.properties");
+            //Создаем объект Properties и загружаем в него данные из файла
+            Properties properties = new Properties ();
+            try {
+                properties.load (new FileReader (file));
+            } catch (IOException e) {
+                e.printStackTrace ();
+            }
+            //проходимся по всем ключам и наполняем таблицу Employees 70 строками
+            for (String key : properties.stringPropertyNames ()) {
+                String sqlCommand = "insert into employees (id, employee_name) values (default, '"
+                        + properties.getProperty (key) + "')";
+                stmt.executeUpdate (sqlCommand);
+            }
+
+            //Закрываем соединение
+            stmt.close ();
+        } catch (SQLException e) {
+            e.printStackTrace ();
+        }
+
+    }
+}
